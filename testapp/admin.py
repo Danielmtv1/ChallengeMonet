5921f558f92c855341cdc770dac60bd6818533d6
danielmtv <danielmtv6@gmail.com>
2023-02-27 22:53:59 -0500

    config settings refactor students add admin

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import StudentManager, Student, Test, Answer, Question, Nota, StudentAnswer
+
+admin.site.register(StudentManager)
+admin.site.register(Student)
+admin.site.register(Test)
+admin.site.register(Answer)
+admin.site.register(Question)
+admin.site.register(Nota)
+admin.site.register(StudentAnswer)
--- testapp/models.py
+++ testapp/models.py
@@ -1,18 +1,61 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import (
+    User,
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
-from django.contrib.auth.models import User
 
 
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
+class StudentManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("email is required")
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+        )
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class Student(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = StudentManager()
 
     def __str__(self):
-        return self.name
+        return self.email
+
+    def get_full_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        return self.email
+
+    def get_short_name(self):
+        if self.first_name:
+            return self.first_name
+        return self.email
+
+    class Meta:
+        verbose_name = "student"
+        verbose_name_plural = "students"
 
 
 class Question(models.Model):
